eddae90ab1bd088a511020a1288b364d2bc16d0a
gottscbe <smarter.engineering@gmail.com>
2021-09-23 14:38:12 +0200

    added comment2 (#2)

--- Class1.cs
+++ Class1.cs
@@ -5,6 +5,7 @@ namespace HelloWorldBg
     public class Class1
     {
         // comment...
+        // comment2...
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
