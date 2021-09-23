aace3d1750b09c0cb3467279b76cd01fed8fe9e6
gottscbe <smarter.engineering@gmail.com>
2021-09-23 14:49:06 +0200

    fix #3

--- Class1.cs
+++ Class1.cs
@@ -5,7 +5,6 @@ namespace HelloWorldBg
     public class Class1
     {
         // comment...
-        // comment2...
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
